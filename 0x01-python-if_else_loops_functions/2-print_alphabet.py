e01ed800bf98634b99c35623b025def97079730a
BcodezMUJJE <billbrightmatege@gmail.com>
2023-06-06 03:57:13 -0700

    alpapbet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 #2-print_alphabet.py
-
 """prints the ASCII alphabet, in lowercase, not followed by a new line."""
 for letters in range(97, 123):
     print("{}".format(chr(letters)), end="")
