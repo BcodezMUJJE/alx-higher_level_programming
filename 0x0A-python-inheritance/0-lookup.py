1c719506779d45aa045077751820e611935ac895
BcodezMUJJE <billbrightmatege@gmail.com>
2023-07-10 11:04:45 +0000

    0. Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    return dir(obj)
+class MyClass1(object):
+    pass
+
+class MyClass2(object):
+    my_attr1 = 3
+    def my_meth(self):
+        pass
+
+print(lookup(MyClass1))
+print(lookup(MyClass2))
+print(lookup(int))
