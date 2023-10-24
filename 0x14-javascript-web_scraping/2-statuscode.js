4938b046a7bae774790e90f8a0d094f45cfeff64
BcodezMUJJE <billbrightmatege@gmail.com>
2023-10-24 05:07:27 -0700

    2-statuscode

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,17 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 
-const url = process.argv[2];
-
-if (!url) {
-  console.log('Usage: node 2-statuscode.js <URL>');
-  process.exit(1);
-}
-
-request(url, (error, response) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(`code: ${response.statusCode}`);
-  }
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
